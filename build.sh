0203e8b6d40d4d398b437362c11665a742f76d67
Roger Lo <sobadrush@icloud.com>
2023-09-10 22:39:26 +0800

    修改 build.sh & docker-compose.yaml

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build --platform linux/amd64 -f ./Dockerfile --no-cache -t ubuntu-ssh:v1 .
+docker build --platform linux/amd64 -f ./Dockerfile --no-cache -t ubuntu:ssh-default .
